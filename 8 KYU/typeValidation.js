2627d7db6282ab174f255e0f437ca30a0d5e88de
Ashley Schuette <ashleyschuette2@gmail.com>
2017-09-22 21:12:02 -0500

    finish typeValidation

---
+++ 8 KYU/typeValidation.js
@@ -0,0 +1,10 @@
+function typeValidation(variable, type) {
+    if (typeof variable == type) {
+      return true
+  } else {
+      return false
+    }
+}
+
+console.log(typeValidation(42, "number")) // true
+console.log(typeValidation("42", "number")) // false
