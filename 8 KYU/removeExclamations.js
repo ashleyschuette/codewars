12b8c322a307f14d9c8d0a6930c770e7160d8593
Ashley Schuette <ashleyschuette2@gmail.com>
2017-09-24 00:08:19 -0500

    fix save issue

--- 8 KYU/removeExclamations.js
+++ 8 KYU/removeExclamations.js
@@ -1,10 +1,7 @@
 // Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string.
 
 function remove(s) {
-    for (var i = 0; i < s.length; i++);
-    if (i === '!') {
-        remove.splice(i)
-    }
+    return s.replace(/!/g, '') + '!'
   }
 
 console.log(remove("Hi!")) // "Hi!"
